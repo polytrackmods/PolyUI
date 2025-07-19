9ef3657d1e0bd343e2532510057a45b5f4d9d896
DoraChad <176503791+DoraChad@users.noreply.github.com>
2025-07-18 23:24:18 -0700

    Update main.mod.js

--- 0.0.0/main.mod.js
+++ 0.0.0/main.mod.js
@@ -1,91 +1,48 @@
 import { PolyMod, MixinType } from "https://pml.orangy.cfd/PolyTrackMods/PolyModLoader/0.5.0/PolyModLoader.js";
 
-class puiMod extends PolyMod {  
-
-  init = function(polyModLoader) {
-
-    this.uiButtons = [];
-    this.uiTabs = [];
-    
-    this.addMenuButton = function(image_path, text, onClick, order = null) {
-      const button = document.createElement("button");
-      button.className = "button button-image";
-      button.innerHTML = `<img src="${image_path}">`;
-  
-      const w = document.createElement("p");
-      w.textContent = text;
-      button.appendChild(w);
-  
-      this.registerElement("mnu", button, onClick, order);
-    };
-    
-    this.addMenuTab = function(text, image_path, onClick) {
-      this.uiTabs.push([text, image_path, onClick]);  
-    };
-    
-    this.registerElement = function(id, element, onClick, extras = null) {
-      this.uiButtons.push([id, element, onClick, extras]);
-      console.log(this.uiButtons);
-    };
-    
-    this.createTabs = function(parent, audio, onclick) {
-      for (let i = 0; i < this.uiTabs.length; i++) {
-        const button = document.createElement("button");
-        button.className = `button ${this.uiTabs[i][0].toLowerCase()}`;
-        
-        button.append(document.createTextNode(`${this.uiTabs[i][0]} tracks`));
-        
-        const cover = document.createElement("div");
-        cover.className = "cover";
-        button.prepend(cover);
-  
-        const style = document.createElement("style");
-        style.textContent = `button.${this.uiTabs[i][0].toLowerCase()}::before {\n\tbackground-image: url(${this.uiTabs[i][1]});\n}`;
-        document.head.appendChild(style);
-  
-        this.uiTabs[i].push(button);
-      }
-    };
-
-    polyModLoader.registerFuncMixin("hD.prototype", MixinType.TAIL, () => {
-      const subdata = this.uiButtons.filter(arr => arr[0] === "mnu");
-      subdata.forEach((item) => {
-        item[1].addEventListener("click",() => {
-            n.playUIClick();
-            item[2]();
-        });
-
-        
-        if (Number.isInteger(item[3]) && wD(this, iD, "f").length == item[3]) {
-            wD(this, nD, "f").appendChild(item[1]);
-        } else {
-            wD(this, nD, "f").insertBefore(item[1], wD(this, nD, "f").children[item[3]-1]);
-        }
-        
-        if (item[3] && Number.isInteger(item[3])) {
-            wD(this, iD, "f").splice(item[3]-1,0,item[1]);
-        } else {
-            wD(this, iD, "f").push(item[1]);
-        }
-
-        
-    });
-    });
-    
-    polyModLoader.registerFuncMixin("$k.prototype", MixinType.INSERT, 'Kk(this, Ck, "f").prepend(m);', () => {
-      this.createTabs();
-      this.uiTabs.forEach((item) => {
-        const button = item[3];
-        button.addEventListener("click", () => {
-          Kk(this, wk, "f").playUIClick();
-          item[2]();
-        });
-        u.appendChild(button);
-      });
-    });
-
+class puiMod extends PolyMod {
+  PUIRoot = function(div, children) {
+    children.forEach((child) => div.appendChild(child));
   }
-
+  applyStyles = function(element, styles = {}) {
+    if (!styles) return;
+    for (const [key, value] of Object.entries(styles)) {
+      element.style[key] = value;
+    }
+  }
+  Column = function(children, styles) {
+    const div = document.createElement("div");
+    div.style.display = "flex";
+    div.style.flexDirection = "column";
+    div.style.gap = "10px";
+    this.applyStyles(div, styles);
+    children.forEach((child) => div.appendChild(child));
+    return div;
+  }
+  Row = function(children, styles) {
+    const div = document.createElement("div");
+    div.style.display = "flex";
+    div.style.flexDirection = "row";
+    div.style.gap = "10px";
+    this.applyStyles(div, styles);
+    children.forEach((child) => div.appendChild(child));
+    return div;
+  }
+  
+  Button = function(label, onClick, styles) {
+    const btn = document.createElement("button");
+    btn.textContent = label;
+    btn.onclick = onClick;
+    this.applyStyles(btn, styles);
+    return btn;
+  }
+  
+  Text = function(content, styles) {
+    const p = document.createElement("p");
+    p.textContent = content;
+    this.applyStyles(p, styles);
+    return p;
+  }
 }
 
 export let polyMod = new puiMod();
